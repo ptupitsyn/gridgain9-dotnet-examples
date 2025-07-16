c2b9610105abd645ec93f819a4693e54042b460a
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-16 10:47:13 +0300

    Moved

--- GridGain9.Examples/DisposableDeploymentUnit.cs
+++
@@ -1,8 +0,0 @@
-using Apache.Ignite.Compute;
-
-namespace GridGain9.ComputeTester;
-
-public sealed record DisposableDeploymentUnit(DeploymentUnit Unit) : IAsyncDisposable
-{
-    public async ValueTask DisposeAsync() => await ManagementApi.UndeployUnit(Unit);
-}
